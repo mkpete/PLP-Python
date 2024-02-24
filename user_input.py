9d8fa9644fab6bff9abe84545234a5a468ec3d80
Muturi_Pete <67792341+mkpete@users.noreply.github.com>
2024-02-24 20:31:06 +0300

    intro

---
+++ user_input.py
@@ -0,0 +1,13 @@
+# program that asks the user for their name, age, and location and then prints out a personalized message.
+
+# Ask for their name
+name = input("What is your name? ")
+
+# Ask for their age
+age = input("How old are you? ")
+
+# Ask for their location
+location = input("Where do you live? ")
+
+# user personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
